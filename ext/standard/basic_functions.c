b3d063202611a34e494d1e57def3c0850090ff72
abrari <muhammad.abrari@gmail.com>
2017-12-02 20:39:35 +0700

    array count -> hitung / ukuran

--- ext/standard/basic_functions.c
+++ ext/standard/basic_functions.c
@@ -3321,7 +3321,7 @@ const zend_function_entry basic_functions[] = { /* {{{ */
 	PHP_FE(shuffle,															arginfo_shuffle)
 	PHP_FE(array_walk,														arginfo_array_walk)
 	PHP_FE(array_walk_recursive,											arginfo_array_walk_recursive)
-	PHP_FE(count,															arginfo_count)
+	PHP_FE(hitung,															arginfo_count)
 	PHP_FE(end,																arginfo_end)
 	PHP_FE(prev,															arginfo_prev)
 	PHP_FE(next,															arginfo_next)
@@ -3385,7 +3385,7 @@ const zend_function_entry basic_functions[] = { /* {{{ */
 
 	/* aliases from array.c */
 	PHP_FALIAS(pos,					current,								arginfo_current)
-	PHP_FALIAS(sizeof,				count,									arginfo_count)
+	PHP_FALIAS(ukuran,				hitung,									arginfo_count)
 	PHP_FALIAS(key_exists,			array_key_exists,						arginfo_array_key_exists)
 
 	/* functions from assert.c */
